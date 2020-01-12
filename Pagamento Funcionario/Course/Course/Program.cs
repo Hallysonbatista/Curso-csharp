7780057a484c77b438e29a4ca3c11b0fcf2908f0
Hallyson Batista <hallyson-1301709@fapi-pinhais.edu.br>
2020-01-12 10:04:49 -0300

    parte 3/3 do exec 120

---
+++ Pagamento Funcionario/Course/Course/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter department's name: ");
+            string depName = Console.ReadLine();
+            Console.WriteLine("Enter worker data: ");
+            Console.WriteLine("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Level: ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.WriteLine("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(depName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            
+            Console.WriteLine("How many contracts to this worker?: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data: ");
+                Console.Write("Data (DD/MM/YYYY): ";
+            }
+        }
+    }
+}
