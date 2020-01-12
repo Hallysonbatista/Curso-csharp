d931284459d948f277b3f2d53ad036f6c8553bad
Hallyson Batista <hallyson-1301709@fapi-pinhais.edu.br>
2020-01-12 20:06:19 -0300

    Exec.

---
+++ Pedidos/Course/Course/Entities/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities.Enum;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public DateTime moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            this.moment = moment;
+            Status = status;
+            Client = client;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
---
+++ Pedidos/Course/Course/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Entities.Enum;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data: ");
+
+        }
+    }
+}
